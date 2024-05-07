1e963ecdd7293214e768dd96a7b609b4708ba89d
Ken18188 <127706238+Ken18188@users.noreply.github.com>
2024-05-07 13:38:14 -0400

    Secure and refactor app configuration

--- app.py
+++ app.py
@@ -5,17 +5,6 @@ from flask import Flask
 import os
 
 
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-
 app = Flask(__name__)
 
 @app.route('/')
