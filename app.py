5da87c49082c59d028ebceac15dd679e42fcc24d
Ken18188 <127706238+Ken18188@users.noreply.github.com>
2024-05-07 17:53:09 -0400

    Updated app.py with changes

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
+import os
+import sys
+
+root_path = os.path.abspath(__file__)
+root_path = '/'.join(root_path.split('/')[:-2])
+sys.path.append(root_path)
+
+
 from apexpro.constants import APEX_HTTP_TEST, APEX_HTTP_MAIN
 from apexpro.http_public import HttpPublic
 
+print("Hello, Apexpro")
+
 client = HttpPublic(APEX_HTTP_MAIN)
-print(client.configs_v2())
 print(client.history_funding_v2(symbol="BTC-USDT"))
 print(client.klines(symbol="ETHUSDT",interval=5,start=1681463600, end=1681563600, limit=5))
 print(client.server_time())
+print(client.configs_v2())
 print(client.depth(symbol="BTC-USDC"))
 print(client.trades(symbol="BTC-USDC"))
 print(client.klines(symbol="BTC-USDT",interval="15"))
