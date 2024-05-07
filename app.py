38449cdc82ea5f749bf8212f5caaf90f773a93e5
Ken18188 <127706238+Ken18188@users.noreply.github.com>
2024-05-07 11:56:10 -0400

    Initial commit with main application script

---
+++ app.py
@@ -0,0 +1,11 @@
+from apexpro.http_private import HttpPrivate
+from apexpro.constants import APEX_HTTP_MAIN, NETWORKID_MAIN
+
+key = '9a8408c6-4292-5513-cb1c-20fcbc4c8077'
+secret = 'QSs6JH3NjLbMsXLP-gl32uDNkE_-QCEKICso4boo'
+passphrase = 'a32muWF4n8RAmLN7JpSq'
+
+client = HttpPrivate(APEX_HTTP_MAIN, network_id=NETWORKID_MAIN, api_key_credentials={'key': key, 'secret': secret, 'passphrase': passphrase})
+
+accountRes = client.get_account_balance()
+print(accountRes)
