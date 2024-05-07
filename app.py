20fe9dfa4d43fead89a1614765d7491654a2f1d9
Ken18188 <127706238+Ken18188@users.noreply.github.com>
2024-05-07 13:35:19 -0400

    Secure and refactor app configuration

--- app.py
+++ app.py
@@ -4,7 +4,17 @@ from apexpro.constants import APEX_HTTP_MAIN, NETWORKID_MAIN
 from flask import Flask
 import os
 
+
+const express = require('express')
+const app = express()
 const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 
 app = Flask(__name__)
 
